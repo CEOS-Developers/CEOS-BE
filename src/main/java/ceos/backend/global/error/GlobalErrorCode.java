603764b6c02a531a3fa295d7881883ba7cf9e0f9
wjdtkdgns <wjdtkdgns329@naver.com>
2023-05-05 18:56:58 +0900

    [feat] 공통 에러 처리, 예시 api 구현 #2

--- src/main/java/ceos/backend/global/error/GlobalErrorCode.java
+++ src/main/java/ceos/backend/global/error/GlobalErrorCode.java
@@ -5,12 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Getter
 @AllArgsConstructor
 public enum GlobalErrorCode implements BaseErrorCode{
 
+    _INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "GLOBAL_500_1", "서버 오류. 관리자에게 문의 부탁드립니다."),
     EXAMPLE_GLOBAL_NOT_FOUND(NOT_FOUND, "EXAMPLEGLOBAL_404_1", "예시를 찾을 수 없는 오류입니다."),
     ;
 
