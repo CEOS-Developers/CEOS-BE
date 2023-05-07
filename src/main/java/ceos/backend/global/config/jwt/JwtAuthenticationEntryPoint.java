b2d7841b63bcb6c4e03e49b256a03f29ac0c7a04
suhhyun <97878992+suhhyun524@users.noreply.github.com>
2023-05-07 22:06:59 +0900

    [add] Exception 적용 #4

--- src/main/java/ceos/backend/global/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/ceos/backend/global/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,22 +1,48 @@
 package ceos.backend.global.config.jwt;
 
+import ceos.backend.global.error.BaseErrorCode;
+import ceos.backend.global.error.ErrorResponse;
+import ceos.backend.global.error.exception.NoTokenException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    
+
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(
-            HttpServletRequest request, 
-            HttpServletResponse response, 
+            HttpServletRequest request,
+            HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        //response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        String exception = (String) request.getAttribute("exception");
+
+        responseToClient(response, getErrorResponse(NoTokenException.EXCEPTION.getErrorCode()));
+
+    }
+
+    private ErrorResponse getErrorResponse(BaseErrorCode errorCode) {
+
+        return ErrorResponse.of(errorCode.getErrorReason());
+    }
+
+    private void responseToClient(HttpServletResponse response, ErrorResponse errorResponse)
+            throws IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(Integer.parseInt(errorResponse.getStatus()));
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
     }
 }
---
+++ src/main/java/ceos/backend/global/error/exception/NoTokenException.java
@@ -0,0 +1,11 @@
+package ceos.backend.global.error.exception;
+
+import ceos.backend.global.error.BaseErrorException;
+import ceos.backend.global.error.GlobalErrorCode;
+
+public class NoTokenException extends BaseErrorException {
+
+    public static final BaseErrorException EXCEPTION = new NoTokenException();
+
+    private NoTokenException() { super(GlobalErrorCode.NO_TOKEN); }
+}
