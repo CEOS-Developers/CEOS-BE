ab29d238d356788bb59a24f66810594140cb6d79
Ayeon Kim <77188666+yourzinc@users.noreply.github.com>
2023-05-09 23:55:45 +0900

    [feat] Swagger 설정  (#15)
    * [chore] Swagger 관련 의존관계 설정
    
    * [chore] Swagger 관련 설정 정보
    
    * [chore] Swagger config 추가
    
    * [fix] Swagger pattern 추가
    
    * [feat] Swagger 사용 예시 API 추가
    
    ---------
    
    Co-authored-by: Sanghoon Jeong <67852689+wjdtkdgns@users.noreply.github.com>

--- src/main/java/ceos/backend/domain/example/ExampleController.java
+++ src/main/java/ceos/backend/domain/example/ExampleController.java
@@ -3,11 +3,19 @@ package ceos.backend.domain.example;
 
 import ceos.backend.domain.example.dto.SlackTest;
 import ceos.backend.domain.example.exception.ExampleNotFoundException;
-import ceos.backend.global.common.dto.SlackErrorMessage;
-import ceos.backend.global.common.event.Event;
+import ceos.backend.global.common.response.SuccessResponse;
+import ceos.backend.global.error.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Example Controller", description = "Example Controller 설명입니다.")
 @RestController
 @RequestMapping(value = "/example")
 @RequiredArgsConstructor
@@ -57,4 +65,31 @@ public class ExampleController {
     public int slack(@RequestParam int a, @RequestBody SlackTest slackTest) throws Exception {
         throw new Exception();
     }
+
+    /**
+     * Swagger 사용 예시 : GET
+     */
+    @Operation(summary = "Swagger GET 메서드", description = "Swagger 메서드 설명입니다.", tags = {"Swagger"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @GetMapping(value = "/swagger")
+    public String swagger() {
+        return "Hi, Swagger";
+    }
+
+    /**
+     * Swagger 사용 예시 : POST
+     */
+    @Operation(summary = "Swagger POST 메서드", description = "Swagger 메서드 설명입니다.", tags = {"Swagger"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = SuccessResponse.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping(value = "/swagger")
+    public String swagger(@Parameter(description = "Swagger") String swagger) {
+        return "Hi," + swagger;
+    }
+
 }
