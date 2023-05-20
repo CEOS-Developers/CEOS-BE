fa53eb35286bef5b252c86b2689fc1957bc5cd5b
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2023-05-20 14:05:14 +0900

    [feat] s3 연동 (#28)

--- src/main/java/ceos/backend/infra/s3/AwsS3Config.java
+++ src/main/java/ceos/backend/infra/s3/AwsS3Config.java
@@ -1,26 +1,24 @@
-package ceos.backend.infra.ses;
+package ceos.backend.infra.s3;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.ses.SesAsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
-@Configuration
-public class AwsSESConfig {
-    @Value("${aws.ses.access-key}")
+public class AwsS3Config {
+    @Value("${aws.s3.access-key}")
     private String AWS_ACCESS_KEY_ID;
 
-    @Value("${aws.ses.secret-key}")
+    @Value("${aws.s3.secret-key}")
     private String AWS_SECRET_KEY;
 
     @Bean
-    public SesAsyncClient sesAsyncClient() {
+    public S3Client s3Client() {
         final AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
         final StaticCredentialsProvider staticCredentialsProvider = StaticCredentialsProvider.create(awsBasicCredentials);
-        return SesAsyncClient.builder()
+        return S3Client.builder()
                 .credentialsProvider(staticCredentialsProvider)
                 .region(Region.AP_NORTHEAST_2)
                 .build();
