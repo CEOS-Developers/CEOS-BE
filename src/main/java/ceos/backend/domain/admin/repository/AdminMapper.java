c7a2be2f2e5134e77f6ee179ed54f35eb1fb6206
suhhyun <97878992+suhhyun524@users.noreply.github.com>
2023-05-23 00:46:08 +0900

    [feat] Admin 관련 메서드 작성 #29

--- src/main/java/ceos/backend/domain/admin/repository/AdminMapper.java
+++ src/main/java/ceos/backend/domain/admin/repository/AdminMapper.java
@@ -2,11 +2,31 @@ package ceos.backend.domain.admin.repository;
 
 import ceos.backend.domain.admin.domain.Admin;
 import ceos.backend.domain.admin.dto.request.SignUpRequest;
+import ceos.backend.domain.admin.dto.response.CheckUsernameResponse;
+import ceos.backend.domain.admin.dto.response.FindIdResponse;
+import ceos.backend.domain.admin.dto.response.RefreshTokenResponse;
+import ceos.backend.domain.admin.dto.response.SignInResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdminMapper {
-    public Admin toEntity(SignUpRequest signUpRequest, String hashedPassword, int generation) {
-        return Admin.of(signUpRequest, hashedPassword, generation);
+    public Admin toEntity(SignUpRequest signUpRequest, String encodedPassword, int generation) {
+        return Admin.of(signUpRequest, encodedPassword, generation);
+    }
+
+    public CheckUsernameResponse toCheckUsernameResponse(boolean isAvailable) {
+        return CheckUsernameResponse.from(isAvailable);
+    }
+
+    public SignInResponse toSignInResponse(String accessToken, String refreshToken) {
+        return SignInResponse.of(accessToken, refreshToken);
+    }
+
+    public FindIdResponse toFindIdResponse(String username) {
+        return FindIdResponse.from(username);
+    }
+
+    public RefreshTokenResponse toRefreshTokenResponse(String accessToken) {
+        return RefreshTokenResponse.from(accessToken);
     }
 }
