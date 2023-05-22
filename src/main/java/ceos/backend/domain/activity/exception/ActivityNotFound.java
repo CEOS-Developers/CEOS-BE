678b6084df4c72ce2b73c0ca962967df38b0ac79
yourzinc <your_zinc@ewhain.net>
2023-05-23 01:59:02 +0900

    [feat] 활동 관련 ErrorCode 작성

---
+++ src/main/java/ceos/backend/domain/activity/exception/ActivityNotFound.java
@@ -0,0 +1,12 @@
+package ceos.backend.domain.activity.exception;
+
+import ceos.backend.global.error.BaseErrorException;
+
+public class ActivityNotFound extends BaseErrorException {
+
+    public static final ActivityNotFound EXCEPTION = new ActivityNotFound();
+
+    public ActivityNotFound() {
+        super(ActivityErrorCode.ACTIVITY_NOT_FOUND);
+    }
+}
