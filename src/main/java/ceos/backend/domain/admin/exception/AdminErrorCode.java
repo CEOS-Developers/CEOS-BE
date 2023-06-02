2165096eb61d4411bb29ae57eed2a76edaeeca02
suhhyun524 <2835223@ewhain.net>
2023-06-03 02:26:28 +0900

    [add] 에러 추가 #52

--- src/main/java/ceos/backend/domain/admin/exception/AdminErrorCode.java
+++ src/main/java/ceos/backend/domain/admin/exception/AdminErrorCode.java
@@ -13,6 +13,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum AdminErrorCode implements BaseErrorCode {
     /* Admin */
     DUPLICATE_ADMIN(BAD_REQUEST, "ADMIN_400_1", "이미 가입한 어드민입니다."),
+    INVALID_ACTION(BAD_REQUEST, "ADMIN_400_2", "자기 자신에 대한 작업은 수행할 수 없습니다."),
     NOT_ALLOWED_TO_MODIFY(FORBIDDEN, "ADMIN_403_1", "지원 기간에는 수정할 수 없습니다."),
     ADMIN_NOT_SIGN_UP(NOT_FOUND, "ADMIN_404_1", "회원으로 가입된 유저가 아닙니다"),
     ADMIN_NOT_FOUND(NOT_FOUND, "ADMIN_404_2", "존재하지 않는 어드민입니다."),
