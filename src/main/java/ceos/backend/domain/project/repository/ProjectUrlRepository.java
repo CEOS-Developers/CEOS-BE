b6ee508ab8ca5bb0e13e2b5d750534548de89406
suhhyun524 <2835223@ewhain.net>
2023-06-05 02:35:06 +0900

    [add] repository 추가 #55

---
+++ src/main/java/ceos/backend/domain/project/repository/ProjectUrlRepository.java
@@ -0,0 +1,8 @@
+package ceos.backend.domain.project.repository;
+
+import ceos.backend.domain.project.domain.ProjectUrl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectUrlRepository extends JpaRepository<ProjectUrl, Long> {
+
+}
