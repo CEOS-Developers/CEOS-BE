7076365f83b4d0810ac22c5471427424b46f3bc9
suhhyun524 <2835223@ewhain.net>
2023-06-05 02:33:01 +0900

    [add] 에러코드 추가 #55

---
+++ src/main/java/ceos/backend/domain/project/exception/DuplicateProject.java
@@ -0,0 +1,12 @@
+package ceos.backend.domain.project.exception;
+
+import ceos.backend.global.error.BaseErrorException;
+
+public class DuplicateProject extends BaseErrorException {
+
+    public static final DuplicateProject EXCEPTION = new DuplicateProject();
+
+    private DuplicateProject() {
+        super(ProjectErrorCode.DUPLICATE_PROJECT);
+    }
+}
--- src/main/java/ceos/backend/domain/project/exception/ProjectErrorCode.java
+++ src/main/java/ceos/backend/domain/project/exception/ProjectErrorCode.java
@@ -12,11 +12,8 @@ import static org.springframework.http.HttpStatus.*;
 @AllArgsConstructor
 public enum ProjectErrorCode implements BaseErrorCode {
     /* Project */
-    DUPLICATE_PROJECT(BAD_REQUEST, "ADMIN_400_1", "이미 가입한 어드민입니다."),
-    INVALID_ACTION(BAD_REQUEST, "ADMIN_400_2", "자기 자신에 대한 작업은 수행할 수 없습니다."),
-    NOT_ALLOWED_TO_MODIFY(FORBIDDEN, "ADMIN_403_1", "지원 기간에는 수정할 수 없습니다."),
-    ADMIN_NOT_SIGN_UP(NOT_FOUND, "ADMIN_404_1", "회원으로 가입된 유저가 아닙니다"),
-    PROJECT_NOT_FOUND(NOT_FOUND, "ADMIN_404_2", "존재하지 않는 프로젝트입니다."),
+    DUPLICATE_PROJECT(BAD_REQUEST, "PROJECT_400_1", "이미 존재하는 프로젝트입니다."),
+    PROJECT_NOT_FOUND(NOT_FOUND, "ADMIN_404_1", "존재하지 않는 프로젝트입니다."),
 
     /* Data */
     DUPLICATE_DATA(CONFLICT, "PROJECT_409_1", "이미 존재하는 데이터입니다");
