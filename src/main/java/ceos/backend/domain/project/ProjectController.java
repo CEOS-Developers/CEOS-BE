a40d3f342458d4f9297fd22cfef572e3b572f9df
suhhyun524 <2835223@ewhain.net>
2023-06-05 02:34:32 +0900

    [feat] 프로젝트 생성 API 구현 #55

--- src/main/java/ceos/backend/domain/project/ProjectController.java
+++ src/main/java/ceos/backend/domain/project/ProjectController.java
@@ -1,16 +1,15 @@
 package ceos.backend.domain.project;
 
+import ceos.backend.domain.project.dto.request.CreateProjectRequest;
 import ceos.backend.domain.project.dto.response.GetProjectResponse;
 import ceos.backend.domain.project.dto.response.GetProjectsResponse;
 import ceos.backend.domain.project.service.ProjectService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -23,9 +22,17 @@ public class ProjectController {
 
     @Operation(summary = "프로젝트 목록 보기")
     @GetMapping("/")
-    public GetProjectsResponse getProjects() {
+    public GetProjectsResponse getProjects(@RequestParam("pageNum") int pageNum,
+                                           @RequestParam("limit") int limit) {
         log.info("프로젝트 목록 보기");
-        return projectService.getProjects();
+        return projectService.getProjects(pageNum, limit);
+    }
+
+    @Operation(summary = "프로젝트 생성하기")
+    @PostMapping("/")
+    public void createProject(@RequestBody @Valid CreateProjectRequest createProjectRequest) {
+        log.info("프로젝트 생성하기");
+        projectService.createProject(createProjectRequest);
     }
 
     @Operation(summary = "프로젝트 하나 보기")
@@ -36,4 +43,12 @@ public class ProjectController {
         log.info("프로젝트 하나 보기");
         return projectService.getProject(projectId);
     }
+
+//    @Operation(summary = "프로젝트 수정하기")
+//    @PatchMapping("/")
+//    public void updateProject(@RequestBody @Valid UpdateProjectRequest updateProjectRequest) {
+//        log.info("프로젝트 수정하기");
+//        projectService.updateProject(updateProjectRequest);
+//    }
+
 }
--- src/main/java/ceos/backend/domain/project/service/ProjectService.java
+++ src/main/java/ceos/backend/domain/project/service/ProjectService.java
@@ -1,31 +1,73 @@
 package ceos.backend.domain.project.service;
 
+import ceos.backend.domain.project.domain.*;
+import ceos.backend.domain.project.dto.request.CreateProjectRequest;
 import ceos.backend.domain.project.dto.response.GetProjectResponse;
 import ceos.backend.domain.project.dto.response.GetProjectsResponse;
 import ceos.backend.domain.project.helper.ProjectHelper;
-import ceos.backend.domain.project.repository.ProjectMapper;
-import ceos.backend.domain.project.repository.ProjectRepository;
+import ceos.backend.domain.project.repository.*;
+import ceos.backend.domain.project.vo.ParticipantVo;
+import ceos.backend.domain.project.vo.ProjectImageVo;
+import ceos.backend.domain.project.vo.ProjectUrlVo;
+import ceos.backend.global.common.dto.PageInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProjectService {
-    
+
     private final ProjectHelper projectHelper;
     private final ProjectMapper projectMapper;
     private final ProjectRepository projectRepository;
+    private final ProjectImageRepository projectImageRepository;
+    private final ProjectUrlRepository projectUrlRepository;
+    private final ParticipantRepository participantRepository;
 
-    @Transactional
-    public GetProjectsResponse getProjects() {
-        return projectMapper.toGetProjects(projectRepository.findAll());
+    @Transactional(readOnly = true)
+    public GetProjectsResponse getProjects(int pageNum, int limit) {
+        PageRequest pageRequest = PageRequest.of(pageNum, limit);
+        Page<Project> projectList = projectRepository.findAll(pageRequest);
+        PageInfo pageInfo = PageInfo.of(pageNum, limit, projectList.getTotalPages(), projectList.getTotalElements());
+        return projectMapper.toGetProjects(projectList, pageInfo);
     }
 
     @Transactional
+    public void createProject(CreateProjectRequest createProjectRequest) {
+        //프로젝트 중복 검사
+        projectHelper.findDuplicateProject(createProjectRequest.getProjectInfoVo());
+
+        //프로젝트 생성
+        final Project project = projectMapper.toEntity(createProjectRequest);
+        projectRepository.save(project);
+
+        //프로젝트 이미지 저장
+        final List<ProjectImageVo> projectImageVos = createProjectRequest.getProjectImages();
+        projectImageRepository.saveAll(projectMapper.toProjectImageList(project, projectImageVos));
+
+        //프로젝트 Url 저장
+        final List<ProjectUrlVo> projectUrlVos = createProjectRequest.getProjectUrls();
+        projectUrlRepository.saveAll(projectMapper.toProjectUrlList(project, projectUrlVos));
+
+        //프로젝트 팀원 저장
+        final List<ParticipantVo> participantVos = createProjectRequest.getParticipants();
+        participantRepository.saveAll(projectMapper.toParticipantList(project, participantVos));
+    }
+
+    @Transactional(readOnly = true)
     public GetProjectResponse getProject(Long projectId) {
         return projectMapper.toGetProject(projectHelper.findById(projectId));
+    }
+
+    @Transactional
+    public void updateProject(Long projectId) {
+
     }
 }
