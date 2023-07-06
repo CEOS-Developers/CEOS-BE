dbd645a003821e0a30a0c37dc79c129880dc628c
suhhyun524 <2835223@ewhain.net>
2023-07-07 01:44:41 +0900

    [feat] DTO 추가 및 수정 #66

---
+++ src/main/java/ceos/backend/domain/admin/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,13 @@
+package ceos.backend.domain.admin.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+public class RefreshTokenRequest {
+
+    @Schema(defaultValue = "string")
+    @NotNull(message = "리프레시 토큰을 입력해주세요.")
+    private String refreshToken;
+}
