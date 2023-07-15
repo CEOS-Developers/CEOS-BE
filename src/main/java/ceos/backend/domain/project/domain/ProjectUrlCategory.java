a30234a9a3270f3a77552d3474223b533ba4887b
suhhyun524 <2835223@ewhain.net>
2023-07-15 15:55:02 +0900

    [refact] Enum 수정 #83

--- src/main/java/ceos/backend/domain/project/domain/ProjectUrlCategory.java
+++ src/main/java/ceos/backend/domain/project/domain/ProjectUrlCategory.java
@@ -1,5 +1,28 @@
 package ceos.backend.domain.project.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.stream.Stream;
+
+@Getter
+@RequiredArgsConstructor
 public enum ProjectUrlCategory {
-    SERVICE, GITHUB, BEHANCE, INSTAGRAM
+    SERVICE("서비스"),
+    GITHUB("깃허브"),
+    BEHANCE("비핸스"),
+    INSTAGRAM("인스타");
+
+    @JsonValue
+    private final String projectUrlCategory;
+
+    @JsonCreator
+    public static ProjectUrlCategory parsing(String inputValue) {
+        return Stream.of(ProjectUrlCategory.values())
+                .filter(category -> category.getProjectUrlCategory().equals(inputValue))
+                .findFirst()
+                .orElse(null);
+    }
 }
