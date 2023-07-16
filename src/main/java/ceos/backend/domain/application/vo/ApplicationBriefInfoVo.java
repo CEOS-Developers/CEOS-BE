a521adb268dc91726833566057ba3b23d004cd48
wjdtkdgns <wjdtkdgns10266@gmail.com>
2023-07-16 21:47:19 +0900

    [feat] 지원자 모두 보기 response 변경 #86

--- src/main/java/ceos/backend/domain/application/vo/ApplicationBriefInfoVo.java
+++ src/main/java/ceos/backend/domain/application/vo/ApplicationBriefInfoVo.java
@@ -3,6 +3,7 @@ package ceos.backend.domain.application.vo;
 import ceos.backend.domain.application.domain.ApplicantInfo;
 import ceos.backend.domain.application.domain.Application;
 import ceos.backend.domain.application.domain.Pass;
+import ceos.backend.global.common.entity.Part;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,16 +13,18 @@ public class ApplicationBriefInfoVo {
     @JsonUnwrapped
     private ApplicantInfo applicantInfo;
 
+    private Part part;
     private Long id;
     private Pass documentPass;
     private Pass finalPass;
     private String interviewTime;
 
     @Builder
-    private ApplicationBriefInfoVo(ApplicantInfo applicantInfo, Long id,
+    private ApplicationBriefInfoVo(ApplicantInfo applicantInfo, Long id, Part part,
                                   Pass documentPass, Pass finalPass, String interviewTime) {
         this.applicantInfo = applicantInfo;
         this.id = id;
+        this.part = part;
         this.documentPass = documentPass;
         this.finalPass = finalPass;
         this.interviewTime = interviewTime;
@@ -31,6 +34,7 @@ public class ApplicationBriefInfoVo {
         return ApplicationBriefInfoVo.builder()
                 .applicantInfo(application.getApplicantInfo())
                 .id(application.getId())
+                .part(application.getApplicationDetail().getPart())
                 .documentPass(application.getDocumentPass())
                 .finalPass(application.getFinalPass())
                 .interviewTime(application.getInterviewDatetime())
