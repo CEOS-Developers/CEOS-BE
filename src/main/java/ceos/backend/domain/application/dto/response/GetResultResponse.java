3c7b800b64ca8a461f4bb98a1d64614fd8c6e01f
Sanghoon Jeong <67852689+wjdtkdgns@users.noreply.github.com>
2023-07-23 15:37:22 +0900

    [refact] 결과 확인 response 변경 (#114)

--- src/main/java/ceos/backend/domain/application/dto/response/GetResultResponse.java
+++ src/main/java/ceos/backend/domain/application/dto/response/GetResultResponse.java
@@ -3,23 +3,49 @@ package ceos.backend.domain.application.dto.response;
 
 import ceos.backend.domain.application.domain.Application;
 import ceos.backend.domain.application.domain.Pass;
+import ceos.backend.domain.recruitment.domain.Recruitment;
+import ceos.backend.global.common.dto.ParsedDuration;
+import ceos.backend.global.util.ParsedDurationConvertor;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 public class GetResultResponse {
     private Pass pass;
 
+    private int generation;
+
+    private String name;
+
+    @JsonUnwrapped
+    private ParsedDuration parsedDuration;
+
+    private LocalDate otDate;
+
     @Builder
-    private GetResultResponse(Pass pass) {
+    private GetResultResponse(Pass pass, int generation, String name, ParsedDuration parsedDuration, LocalDate otDate) {
         this.pass = pass;
+        this.generation = generation;
+        this.name = name;
+        this.parsedDuration = parsedDuration;
+        this.otDate = otDate;
     }
 
-    public static GetResultResponse toDocumentResult(Application application) {
-        return GetResultResponse.builder().pass(application.getDocumentPass()).build();
+    public static GetResultResponse toDocumentResult(Application application, Recruitment recruitment) {
+        return GetResultResponse.builder().pass(application.getDocumentPass())
+                .generation(recruitment.getGeneration())
+                .name(application.getApplicantInfo().getName())
+                .parsedDuration(ParsedDurationConvertor.parsingDuration(application.getInterviewDatetime()))
+                .otDate(recruitment.getOtDate()).build();
     }
 
-    public static GetResultResponse toFinalResult(Application application) {
-        return GetResultResponse.builder().pass(application.getFinalPass()).build();
+    public static GetResultResponse toFinalResult(Application application, Recruitment recruitment) {
+        return GetResultResponse.builder().pass(application.getFinalPass()).generation(recruitment.getGeneration())
+                .name(application.getApplicantInfo().getName())
+                .parsedDuration(ParsedDurationConvertor.parsingDuration(application.getInterviewDatetime()))
+                .otDate(recruitment.getOtDate()).build();
     }
 }
