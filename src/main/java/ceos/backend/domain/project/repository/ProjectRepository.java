e704cf557680cdfe563873138a9306aec2ad267f
suhhyun524 <2835223@ewhain.net>
2023-08-05 22:25:19 +0900

    [refact] 프로젝트 순서 변경 #124

--- src/main/java/ceos/backend/domain/project/repository/ProjectRepository.java
+++ src/main/java/ceos/backend/domain/project/repository/ProjectRepository.java
@@ -4,14 +4,19 @@ package ceos.backend.domain.project.repository;
 import ceos.backend.domain.project.domain.Project;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Page<Project> findAllByOrderByIdDesc(PageRequest pageRequest);
+
     Optional<Project> findByNameAndGeneration(String name, int generation);
 
     List<Project> findByGeneration(int generation);
-
+    
     @Query("SELECT MAX(p.generation) FROM Project p")
     int findMaxGeneration();
 }
--- src/main/java/ceos/backend/domain/project/service/ProjectService.java
+++ src/main/java/ceos/backend/domain/project/service/ProjectService.java
@@ -37,7 +37,8 @@ public class ProjectService {
     @Transactional(readOnly = true)
     public GetProjectsResponse getProjects(int pageNum, int limit) {
         PageRequest pageRequest = PageRequest.of(pageNum, limit);
-        Page<Project> projectList = projectRepository.findAll(pageRequest);
+        Page<Project> projectList = projectRepository.findAllByOrderByIdDesc(pageRequest);
+        System.out.println(projectList);
         PageInfo pageInfo =
                 PageInfo.of(
                         pageNum,
