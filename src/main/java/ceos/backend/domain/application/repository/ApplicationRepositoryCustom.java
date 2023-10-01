920eb9ef549cda7f4e11e4c9ab9d3adf5c53d2b3
mirageoasis <kimhw0820@naver.com>
2023-10-02 00:35:39 +0900

    feat : 이름 검색 쿼리 추가

---
+++ src/main/java/ceos/backend/domain/application/repository/ApplicationRepositoryCustom.java
@@ -0,0 +1,17 @@
+package ceos.backend.domain.application.repository;
+
+
+import ceos.backend.domain.application.domain.Application;
+import ceos.backend.domain.application.domain.Pass;
+import ceos.backend.global.common.entity.Part;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+public interface ApplicationRepositoryCustom {
+    Page<Application> findApplications(
+            Part part,
+            Pass docPass,
+            Pass finalPass,
+            String applicantName,
+            PageRequest pageRequest);
+}
