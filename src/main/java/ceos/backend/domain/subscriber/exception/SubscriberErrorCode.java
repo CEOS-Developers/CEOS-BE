98707ff14356849f5950d984d690ca1c9306bc14
yoonsseo <yellisa2000@gmail.com>
2024-08-14 14:27:42 +0900

    [refact] 리크루팅 기간 이외 작업에 대한 예외 변경 및 추가 #192

--- src/main/java/ceos/backend/domain/subscriber/exception/SubscriberErrorCode.java
+++ src/main/java/ceos/backend/domain/subscriber/exception/SubscriberErrorCode.java
@@ -13,7 +13,8 @@ import static org.springframework.http.HttpStatus.*;
 public enum SubscriberErrorCode implements BaseErrorCode {
 
     /* Data */
-    INVALID_ACTION(BAD_REQUEST, "SUBSCRIBER_400_1", "리쿠르팅 시작 전입니다."),
+    INVALID_ACTION_BEFORE(BAD_REQUEST, "SUBSCRIBER_400_1", "리쿠르팅 시작 전입니다."),
+    INVALID_ACTION_AFTER(BAD_REQUEST, "SUBSCRIBER_400_2", "리쿠르팅 마감 후입니다."),
     DUPLICATE_DATA(CONFLICT, "SUBSCRIBER_409_1", "이미 존재하는 데이터입니다");
 
      private HttpStatus status;
