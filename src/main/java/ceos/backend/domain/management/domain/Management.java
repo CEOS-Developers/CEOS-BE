b8d123ac9f3ca1edaf8b2ca473a853643e663ff4
limgahyun <limgh2002@gmail.com>
2025-02-26 03:48:55 +0900

    chore: 주석

--- src/main/java/ceos/backend/domain/management/domain/Management.java
+++ src/main/java/ceos/backend/domain/management/domain/Management.java
@@ -1,6 +1,5 @@
 package ceos.backend.domain.management.domain;
 
-
 import ceos.backend.domain.management.dto.request.UpdateManagementRequest;
 import ceos.backend.domain.management.vo.ManagementVo;
 import ceos.backend.global.common.entity.BaseEntity;
